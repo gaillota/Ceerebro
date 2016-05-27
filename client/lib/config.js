2b70924906f37a337fd8715bd1954286bcfc9be1
gaillota <manzagregine@gmail.com>
2016-05-27 20:20:56 +0200

    Adding SEO config and app capable functionnality

--- client/lib/config.js
+++ client/lib/config.js
@@ -1,3 +1,20 @@
+Meteor.startup(function() {
+    return SEO.config({
+        title: 'Ceerebro',
+        meta: {
+            'description': 'Ceerebro is a secure credentials storage platform',
+            'viewport': 'width=device-width, initial-scale=1, maximum-scale=1',
+            'mobile-web-app-capable': 'yes',
+            'apple-mobile-web-app-capable': 'yes'
+        },
+        link: {
+            'icon': 'img/icons/favicon.ico',
+            'apple-touch-icon': 'img/icons/favicon.ico',
+            'manifest': 'manifest.json'
+        }
+    });
+});
+
 // Autoform error hook
 AutoForm.addHooks(null, {
     onError: function (formType, error) {
@@ -11,6 +28,7 @@ AutoForm.addHooks(null, {
     }
 });
 
+// Hide spinner with for NProgress bar
 NProgress.configure({
     showSpinner: false
 });
