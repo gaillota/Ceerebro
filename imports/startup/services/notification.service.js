72960fabec7c4c5c9a0ca4b705e8fcb6fc1505d5
gaillota <manzagregine@gmail.com>
2016-11-06 17:22:52 +0100

    Bulma - Login + Notification service updated

--- imports/startup/services/notification.service.js
+++ imports/startup/services/notification.service.js
@@ -2,12 +2,29 @@ import { sAlert } from "meteor/juliancwirko:s-alert";
 
 export const Notification = (function() {
     let functions = {};
+    const types = [
+        'success',
+        'info',
+        'warning',
+        'error'
+    ];
 
-    _.each('success info warning error'.split(' '), (type) => {
+    types.forEach((type) => {
         functions[type] = (message, options = {}) => {
+            options.timeout = computeTimeout(message);
             sAlert[type](message, options);
         }
     });
+
+    const computeTimeout = (text) => {
+        const awpm = 200;
+        const minTime = 3000;
+
+        const words = text.trim().split(' ').length;
+        const time = (words / awpm) * 60 * 1000;
+
+        return Math.max(time, minTime);
+    };
 
     return functions;
 })();
