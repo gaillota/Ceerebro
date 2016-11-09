5a69d908ce3066fb5347e63d751b004ae1cf4445
agaillot <agaillot@aubay.com>
2016-11-09 15:14:05 +0100

    Updating index page

--- imports/ui/public/index.js
+++ imports/ui/public/index.js
@@ -13,5 +13,29 @@ Template["public.index"].onCreated(function indexOnCreated() {
 Template["public.index"].helpers({
     countUsers() {
         return Counts.get('count.users');
+    },
+    panelContent() {
+        return [
+            {
+                icon: 'key',
+                title: 'Secured',
+                text: 'Using AES-256 encryption, Ceerebro provides you a secure solution to keep all of your credentials in one place. Only you can access them using your personal master key.'
+            },
+            {
+                icon: 'globe',
+                title: 'Everywhere',
+                text: 'You can access your credentials from anywhere around the world. Even when you are not home, Ceerebro follows you anywhere.'
+            },
+            {
+                icon: 'mobile',
+                title: 'Mobile',
+                text: 'With its responsive design and web app capable feature, you can attach Ceerebro to your mobile phone and make it even easier to access your passwords.'
+            },
+            {
+                icon: 'universal-access',
+                title: 'Accessibility',
+                text: 'Easy-to-use, Ceerebro is a turnkey solution for all of your every day connection issues. Just remember one password, and Ceerebro takes care of the rest.'
+            }
+        ]
     }
 });
