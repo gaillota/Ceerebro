8e0cd5551837e212f050c6ab81e42d5679225be5
gaillota <manzagregine@gmail.com>
2016-11-19 21:16:52 +0100

    Adding navbar toggle logic

--- imports/ui/components/navbar.js
+++ imports/ui/components/navbar.js
@@ -1,6 +1,7 @@
 import {Meteor} from 'meteor/meteor';
 import {Template} from 'meteor/templating';
 import {FlowRouter} from 'meteor/kadira:flow-router';
+import {ReactiveDict} from 'meteor/reactive-dict';
 
 import {Notification} from '../../startup/services/notification.service.js';
 import {showMasterPasswordModal, removeMasterKey} from '../../startup/utilities/functions';
@@ -9,7 +10,23 @@ import './logo';
 
 import './navbar.html';
 
-Template.navbar.events({
+Template["navbar"].onCreated(function onCreatedFunction() {
+    this.state = new ReactiveDict();
+    this.state.set('navbar-toggle', false);
+});
+
+Template["navbar"].helpers({
+    navbarToggle() {
+        return Template.instance().state.get('navbar-toggle') && 'is-active';
+    }
+});
+
+Template["navbar"].events({
+    'click #nav-toggle'(event, instance) {
+        event.preventDefault();
+
+        instance.state.set('navbar-toggle', !instance.state.get('navbar-toggle'));
+    },
     'click .js-set-master-key'(event) {
         event.preventDefault();
 
