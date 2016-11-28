593cfccb3a1a6ee95c973e6a0ef084396b6cfcea
gaillota <manzagregine@gmail.com>
2016-11-28 22:40:04 +0100

    Adding gelocation on map

--- imports/ui/rea/map/index.js
+++ imports/ui/rea/map/index.js
@@ -1,14 +1,17 @@
 import {Template} from "meteor/templating";
+import {ReactiveVar} from 'meteor/reactive-var';
 
 import {Notification} from '../../../startup/services/notification.service';
 import {VelibService} from '../../../startup/services/velib.service';
 import {MapService} from '../../../startup/services/map.service';
+import {GeocodingService} from '../../../startup/services/geocoding.service';
 
 import './components/popup';
 import './index.html';
 
 Template["rea.map.index"].hooks({
     created() {
+        this.location = new ReactiveVar();
     },
     rendered() {
         const map = MapService.loadMap('map');
@@ -20,19 +23,20 @@ Template["rea.map.index"].hooks({
 
         map.on('locationfound', (e) => {
             const radius = e.accuracy / 6;
+
+            this.location.set({lat: e.latitude, lng: e.longitude});
             L.circle(e.latlng, radius).addTo(map);
         });
 
-        VelibService.fetchStations('Paris')
-            .then(response => response.json())
-            .then(data => {
-                const markers = MapService.getMarkerClusters(data);
-                map.addLayer(markers);
-            })
-            .catch((error) => {
-                Notification.error('Could not retrieve stations...');
-                console.log(error);
-            });
+        GeocodingService.geocode("4 allée de la Ferme", (data) => {
+            console.log(data);
+        });
+
+        VelibService.fetchStations('Paris', data => {
+            const markers = MapService.getMarkerClusters(data);
+            map.addLayer(markers);
+        });
+
     }
 });
 
