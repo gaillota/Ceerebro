5cea9fdb7e5c8d7e894fe7bfb70a39c6697a8919
gaillota <manzagregine@gmail.com>
2016-11-28 22:41:23 +0100

    Updating velib service + adding geocoding service

---
+++ imports/startup/services/geocoding.service.js
@@ -0,0 +1,51 @@
+import {_} from 'lodash';
+
+import {Notification} from './notification.service';
+
+export const GeocodingService = (function () {
+    const apiKey = Meteor.settings.public.api.google.apiKey;
+    const baseUrl = "https://maps.googleapis.com/maps/api/geocode/json";
+
+    /**
+     * Geocode address string into coordinate
+     *
+     * @param address
+     * @param handler
+     */
+    function geocode(address, handler) {
+        const params = {
+            address: address,
+            apiKey: apiKey
+        };
+        const url = baseUrl + _urlEncode(params);
+
+        fetch(url)
+            .then(res => res.json())
+            .then(handler)
+            .catch(error => {
+                Notification.error('An error occured while calling Google API...');
+                console.log(error);
+            });
+    }
+
+    /**
+     *
+     * @param params
+     * @returns {string}
+     * @private
+     */
+    function _urlEncode(params) {
+        let path = "?";
+
+        _.forEach(params, (value, key) => {
+            path += `${key}=${encodeURIComponent(value)}&`;
+        });
+        path.slice(1);
+
+        return path;
+    }
+
+    return {
+        geocode: geocode
+    };
+})();
--- imports/startup/services/velib.service.js
+++ imports/startup/services/velib.service.js
@@ -1,20 +1,33 @@
 import 'whatwg-fetch';
 
+import {Notification} from './notification.service';
+
 export const VelibService = (function () {
-    const baseUrl = "https://api.jcdecaux.com/vls/v1";
     const apiKey = Meteor.settings.public.api.velib.apiKey;
+    const baseUrl = "https://api.jcdecaux.com/vls/v1";
 
     /**
      * Fetch stations from API and return an ES6 Promise
      *
      * @param contract
-     * @returns {*|Promise.<string>}
+     * @param handler
      */
-    function fetchStations(contract = 'Paris') {
+    function fetchStations(contract = 'Paris', handler) {
+        if (!apiKey) {
+            Notification.error('No API key for JCDecaux API. Please provide one.');
+            return;
+        }
+
         const path = '/stations?contract=' + contract;
         const url = baseUrl + path + '&apiKey=' + apiKey;
 
-        return fetch(url);
+        fetch(url)
+            .then(res => res.json())
+            .then(handler)
+            .catch((error) => {
+                Notification.error('Could not retrieve stations...');
+                console.log(error);
+            });
     }
 
     return {
